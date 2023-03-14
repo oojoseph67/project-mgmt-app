51ee63e9c7a9887e862c0002e2602070c5404c7e
Obinna Okolo <oojoseph67@gmail.com>
2023-03-14 23:35:24 +0100

    server side

---
+++ server/sampleData.js
@@ -0,0 +1,78 @@
+// Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a leo quis dui gravida convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a leo quis dui gravida convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "NFT Marketplace",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a leo quis dui gravida convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "DEX",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a leo quis dui gravida convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    status: "In Progress",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Amazon Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a leo quis dui gravida convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "johndoe12@gmail.com",
+    phone: "223-334-8900",
+  },
+  {
+    id: "2",
+    name: "Joseph McQu33n",
+    email: "mcQu33n@gmail.com",
+    phone: "899-134-0900",
+  },
+  {
+    id: "3",
+    name: "James Bond",
+    email: "bond007@gmail.com",
+    phone: "125-364-0500",
+  },
+  {
+    id: "4",
+    name: "Leo Messi",
+    email: "theGOAT8@gmail.com",
+    phone: "888-888-8888",
+  },
+  {
+    id: "5",
+    name: "Fraud Ronaldo",
+    email: "fraud@gmail.com",
+    phone: "555-555-5555",
+  },
+];
+
+module.exports = { projects, clients };
